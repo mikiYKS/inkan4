3290483749eed0cec688cacdec4e3ce2b7c1eddd
mikiYKS <103637512+mikiYKS@users.noreply.github.com>
2022-12-16 09:22:33 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+  $("#run").click(() => tryCatch(getKakuin));
+});
+
+function getKakuin() {
+  var authenticator;
+  var client_id = "62eefb0c-1035-4f17-bcc3-715710cbd83d";
+  var redirect_url = "https://mikiyks.github.io/inkan4/";
+  var scope = "https://graph.microsoft.com/Sites.Read.All";
+  var access_token;
+
+  authenticator = new OfficeHelpers.Authenticator();
+
+  //access_token取得
+  authenticator.endpoints.registerMicrosoftAuth(client_id, {
+    redirectUrl: redirect_url,
+    scope: scope
+  });
+
+  authenticator
+    .authenticate(OfficeHelpers.DefaultEndpoints.Microsoft)
+    .then(function (token) {
+      access_token = token.access_token;
+      //API呼び出し
+      $(function () {
+        $.ajax({
+          url:
+            "https://graph.microsoft.com/v1.0/sites/20531fc2-c6ab-4e1e-a532-9c8e15afed0d/drive/items/01SG44IHMJY6HM4OB2XJGZ34EYB77ZANB2",
+          type: "GET",
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer " + access_token);
+          }
+        }).then(
+          async function (data) {
+            const obj = data["@microsoft.graph.downloadUrl"];
+            var kakuinbase64 = await getImageBase64(obj);
+
+            //ここからkakuinbase64を張り付ける処理
+            inkanpaste(kakuinbase64);
+
+          },
+          function (data) {
+            console.log(data);
+          }
+        );
+      });
+    })
+    .catch(OfficeHelpers.Utilities.log);
+}
+
+// バイナリ画像をbase64で返す
+async function getImageBase64(url) {
+  const response = await fetch(url);
+  const contentType = response.headers.get("content-type");
+  const arrayBuffer = await response.arrayBuffer();
+  let base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(arrayBuffer)));
+  //return `data:${contentType};base64,${base64String}`;
+  return base64String;
+}
+
+async function tryCatch(callback) {
+  try {
+    await callback();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+Office.initialize = function (reason) {
+  if (OfficeHelpers.Authenticator.isAuthDialog()) return;
+};
+
+async function inkanpaste(pic) {
+  await Word.run(async (context) => {
+    context.document.getSelection().insertInlinePictureFromBase64(pic, "End");
+    await context.sync();
+  });
+}
