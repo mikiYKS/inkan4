58f95d73c8e4b2adf4af7d0ee84fb7237631e568
mikiYKS <103637512+mikiYKS@users.noreply.github.com>
2022-12-16 19:12:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function getKakuin() {
                 var fileName;
                 var inkanName;
                 if (fileUrl == "") {
-                  fileName = '未保存';
+                  fileName = '未保存ワード';
                   inkanName = "角印";
                 } else {
                   fileName = fileUrl.match(".+/(.+?)([\?#;].*)?$")[1];
