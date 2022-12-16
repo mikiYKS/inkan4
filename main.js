61f6d9e4b66a8d46018179cf69559ce6d4fca078
mikiYKS <103637512+mikiYKS@users.noreply.github.com>
2022-12-16 17:38:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function getKakuin() {
   var authenticator;
   var client_id = "2b588edd-4adc-4e4f-a3e6-a8d362246a29";
   var redirect_url = "https://mikiyks.github.io/inkan4/";
-  var scope = "https://graph.microsoft.com/Sites.Read.All";
+  var scope = "https://graph.microsoft.com/Files.Read.All";
   var access_token;
 
   authenticator = new OfficeHelpers.Authenticator();
@@ -37,6 +37,15 @@ function getKakuin() {
 
             //ここからkakuinbase64を張り付ける処理
             inkanpaste(kakuinbase64);
+
+            //ログ出力
+            Word.run(async (context) => {
+              context.document.load("name");
+              await context.sync();
+              const fileName = context.document.name;
+              const inkanName = "角印";
+              inkanLog(inkanName, fileName);
+            });
 
           },
           function (data) {
@@ -74,5 +83,50 @@ async function inkanpaste(pic) {
   await Word.run(async (context) => {
     context.document.getSelection().insertInlinePictureFromBase64(pic, "End");
     await context.sync();
+  });
+}
+
+
+//SharePointListにログ出力
+function inkanLog(inkanName, inkanFile) {
+  var authenticator;
+  var client_id = "2b588edd-4adc-4e4f-a3e6-a8d362246a29";
+  var redirect_url = "https://mikiyks.github.io/inkan4/";
+  var scope = "https://graph.microsoft.com/Sites.ReadWrite.All";
+  var access_token;
+
+  authenticator = new OfficeHelpers.Authenticator();
+
+  //access_token取得
+  authenticator.endpoints.registerMicrosoftAuth(client_id, {
+    redirectUrl: redirect_url,
+    scope: scope
+  });
+
+  authenticator.authenticate(OfficeHelpers.DefaultEndpoints.Microsoft).then(function (token) {
+    access_token = token.access_token;
+
+    $(function () {
+      $.ajax({
+        url:
+          "https://graph.microsoft.com/v1.0/sites/20531fc2-c6ab-4e1e-a532-9c8e15afed0d/lists/6aac0560-622e-4ee1-ba8f-73b32d8e9f05/items",
+        type: "POST",
+        data: JSON.stringify({
+          fields: {
+            Title: inkanName,
+            FileName: inkanFile
+          }
+        }),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader("Authorization", "Bearer " + access_token);
+        }
+      }).then(
+        async function (data) { },
+        function (data) {
+          console.log(data);
+        }
+      );
+    });
   });
 }
